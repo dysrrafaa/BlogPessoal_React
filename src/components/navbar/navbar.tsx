45102e6326ac34d43abdaa2a8326bc226aa593a3
Rafael <raaf.marques019@gmail.com>
2023-06-16 14:38:15 -0300

    Update - Blog Pessoal (preview)

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {AppBar, Toolbar, Typography, Box} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './navbar.css'
 
 function Navbar (){
     return(
@@ -33,11 +35,13 @@ function Navbar (){
                                 cadastrar tema
                             </Typography>
                         </Box>
+                        <Link to='/login' className='text-decorator-none'>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 logout
                             </Typography>
                         </Box>
+                        </Link>
                     </Box>
 
                 </Toolbar>
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,1 +1,36 @@
 import React from "react";
+import './login.css'
+import { Grid, Box, Typography, TextField, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+function Login() {
+
+    return(
+        
+        <Grid container direction='row' justifyContent="center" alignItems="center">
+            <Grid xs={6} alignItems="center">
+                <Box paddingX={20}>
+                    <form>
+                        |<Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight:'bold'}}>Entrar</Typography>
+                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth/>
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth/>
+                        <Box marginTop={2} textAlign='center'>
+                            <Link to='/home' className='text-decorator-none'> 
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
+                            </Link>
+                        </Box>
+                    </form>
+                </Box>
+            </Grid>
+
+            <Grid xs={6}>
+
+            </Grid>
+        </Grid>
+    );
+
+}
+
+export default Login;
