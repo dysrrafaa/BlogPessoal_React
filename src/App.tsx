87a94749da3758414a364daecb92d609edf60019
Rafael <raaf.marques019@gmail.com>
2023-06-23 12:09:36 -0300

    CRUD completo - BLOG | RM

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/static/navbar/Navbar'
-import Login from './pages/login/Login'
-import Home from './pages/home/Home'
 import Footer from './components/static/footer/Footer'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listaTema/ListaTema';
+import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPostagem/CadastroPostagem';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
-        <Routes> 
-          // Antigo Switch
-          { <Route path="/" element={<Login />} /> }
-          { <Route path="/login" element={<Login />} /> }
+        <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
         </Routes>
       </div>
       <Footer />
-    </BrowserRouter>
+    </Router>
   )
 }
 
---
+++ src/components/temas/cadastroTema/CadastroTema.tsx
@@ -0,0 +1,88 @@
+import React, {useState, useEffect, ChangeEvent} from 'react'
+import { Container, Typography, TextField, Button } from "@material-ui/core"
+import {useNavigate, useParams } from 'react-router-dom'
+import './CadastroTema.css';
+import useLocalStorage from 'react-use-localstorage';
+import Tema from '../../../models/Temas';
+import { buscaId, post, put } from '../../../services/Service';
+
+
+function CadastroTema() {
+    const navigate = useNavigate();
+    const { id } = useParams<{id: string}>();
+    const [token, setToken] = useLocalStorage('token');
+    const [tema, setTema] = useState<Tema>({
+        id: 0,
+        descricao: ''
+    })
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+            navigate("/login")
+    
+        }
+    }, [token])
+
+    useEffect(() =>{
+        if(id !== undefined){
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/temas/${id}`, setTema, {
+            headers: {
+              'Authorization': token
+            }
+          })
+        }
+
+        function updatedTema(e: ChangeEvent<HTMLInputElement>) {
+
+            setTema({
+                ...tema,
+                [e.target.name]: e.target.value,
+            })
+        }
+        
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+            e.preventDefault()
+    
+            if (id !== undefined) {
+                put(`/temas`, tema, setTema, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                alert('Tema Atualizado com Sucesso');
+            } else {
+                post(`/temas`, tema, setTema, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                alert('Tema Cadastrado com Sucesso');
+            }
+            back()
+    
+        }
+    
+        function back() {
+            navigate('/temas')
+        }
+  
+    return (
+        <Container maxWidth="sm" className="topo">
+            <form onSubmit={onSubmit}>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro tema</Typography>
+                <TextField value={tema.descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)} id="descricao" label="Titulo" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <Button type="submit" variant="contained" color="primary">
+                    Finalizar
+                </Button>
+            </form>
+        </Container>
+    )
+}
+
+export default CadastroTema;
