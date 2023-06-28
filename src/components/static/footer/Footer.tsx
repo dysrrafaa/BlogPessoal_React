acb1817ea6c601a6d16e29f66bc6a624e3cd8aaf
Rafael <raaf.marques019@gmail.com>
2023-06-28 07:19:11 -0300

    Footer - Component Completo

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -2,44 +2,57 @@ import React from 'react';
 import InstagramIcon from '@material-ui/icons/Instagram'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import { Typography,Box,Grid } from '@material-ui/core';
+import { Typography, Box, Grid } from '@material-ui/core';
 import './Footer.css'
-import { useSelector } from 'react-redux'
-import { TokenState } from '../../../store/tokens/tokenReducer'
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokenReducer';
 
 function Footer() {
-    return (
-        <>
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    let footerComponent;
+
+    if (token != "") {
+
+        footerComponent =
+
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#ff50508f", height: "120px" }}>
+                    <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                             <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Redes Sociais: </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/dysrrafaa" target="_blank">
-                                <GitHubIcon style={{ fontSize: 40, color: "white" }} />
+                                <GitHubIcon className='redes' />
                             </a>
                             <a href="https://www.instagram.com/dys_rrafaa/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 50, color: "white" }} />
+                                <InstagramIcon className='redes' />
                             </a>
                             <a href="https://www.linkedin.com/in/rafael-marques019/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 50, color: "white" }} />
+                                <LinkedInIcon className='redes' />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#fd4e4e42", height: "00px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >Desenvolvido por Rafael Marques - 2023 © Copyright<br></br> Todos os Direitos Reservados.</Typography>
-                        </Box>
-                        <Box>
-
-                        </Box>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2023 Desenvolvido por Rafeal Marques <br/> Todos os Direitos Reservados. </Typography>
                     </Box>
-                </Grid>
+                </Box>
             </Grid>
+        </Grid>
+
+    }
+
+    return (
+        <>
+            {footerComponent}
         </>
     )
+
 }
 
 export default Footer;
