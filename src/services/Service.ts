08fe378e6fcc814aa8cbe6ec46870a62aac0a61d
Rafael <raaf.marques019@gmail.com>
2023-07-04 09:11:06 -0300

    Deploy - Vercel (VITE_API_URL)

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://blogpessoal-hayo.onrender.com/'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
